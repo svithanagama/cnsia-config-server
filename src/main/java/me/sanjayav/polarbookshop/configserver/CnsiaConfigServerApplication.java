863983a5c5d35471181ce828b8ed533343716be3
sanjayav <svithanagama@gmail.com>
2024-02-20 21:58:03 +1100

    cnsia: Config server initial commit.

---
+++ src/main/java/me/sanjayav/polarbookshop/configserver/CnsiaConfigServerApplication.java
@@ -0,0 +1,15 @@
+package me.sanjayav.polarbookshop.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CnsiaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsiaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sanjayav/polarbookshop/configserver/CnsiaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sanjayav.polarbookshop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsiaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
